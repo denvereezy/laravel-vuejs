ce2fd8b7f680fab8041d9439e71331d304f28e6a
denver <denver@projectcodex.co>
2016-11-27 20:34:00 +0200

    app providers

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Task' => 'App\Policies\TaskPolicy',
+        'App\User' => 'App\Policies\UserPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+    }
+}
